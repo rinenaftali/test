0af3228e8e934d596307ea1a71d1f51f6b5ec5bb
rinenaftali <naftali@cclearly.com>
2015-07-15 17:30:39 +0300

    ss

--- ui-pre.sh
+++ ui-pre.sh
@@ -2,8 +2,9 @@
 if  [[ $CIRCLE_BRANCH == master ]] || [[ $CIRCLE_BRANCH == ui-* ]] ; then
     npm install -g gulp
     npm install -g bower
-    cd ui && npm install
-    cd ui && bower install
+    cd ui 
+    npm install
+    bower install
     sudo apt-get update && sudo apt-get install --only-upgrade google-chrome-stable
 fi
 
